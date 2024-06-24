3aa90c3ae9adcfd4cead92adb12a4ea8f60e3551
kinyarasam <skinyara.30@gmail.com>
2024-06-24 14:18:25 +0300

    String representation of the models.

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
 from sqlalchemy import Column, String, DATETIME
 from datetime import datetime
 import models
 from uuid import uuid4
 
 
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()
 t_format = "%Y-%m-%dT%H:%M:%S%z"
 
 
@@ -37,6 +38,9 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            
+    def __str__(self) -> str:
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.to_dict())
 
     def save(self):
         self.updated_at = datetime.now()
