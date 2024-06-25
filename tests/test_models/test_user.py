ee7ce75ce0804500e7eafc6d2e249df3a8faad5c
kinyarasam <skinyara.30@gmail.com>
2024-06-25 20:09:48 +0300

    # Update tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,6 @@
 import unittest
 import inspect
+import pep8
 from models import user
 from models.base_model import BaseModel
 
@@ -11,7 +12,39 @@ class TestUserDocs(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.user_f = inspect.getmembers(User, inspect.isfunction)
-        
+
+    def test_pep8_conformance_user(self):
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/user/user.py',
+                                    'models/user/__init__.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_user(self):
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_user_module_docstring(self):
+        self.assertIsNot(user.__doc__, None,
+                         "user.py needs a docsctring")
+        self.assertTrue(len(user.__doc__) >= 1,
+                        "user.py needs a docstring")
+
+    def test_user_class_docstring(self):
+        self.assertIsNot(User.__doc__, None,
+                         "User class needs a docstring")
+        self.assertTrue(len(User.__doc__) >= 1,
+                        "User class needs a docstring")
+
+    def test_user_func_docstrings(self):
+        for func in self.user_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[1]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[1]))
+
 
 class TestUser(unittest.TestCase):
     def test_is_subclass(self):
@@ -20,12 +53,12 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(user, "id"))
         self.assertTrue(hasattr(user, "created_at"))
         self.assertTrue(hasattr(user, "updated_at"))
-        
+
     def test_email_attribute(self):
         user = User()
         self.assertTrue(hasattr(user, "email"))
         self.assertEqual(user.email, None)
-        
+
     def test_first_name_attribute(self):
         user = User()
         self.assertTrue(hasattr(user, "first_name"))
@@ -40,12 +73,12 @@ class TestUser(unittest.TestCase):
         user = User()
         self.assertTrue(user, "password")
         self.assertEqual(user.password, None)
-        
+
     def test_role_attribute(self):
         user = User()
         self.assertTrue(user, "role")
         self.assertEqual(user.role, None)
-        
+
     def test_to_dict_creates_dict(self):
         u = User()
         new_d = u.to_dict()
@@ -65,7 +98,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(new_d["updated_at"]), str)
         self.assertEqual(new_d["created_at"], u.created_at.strftime(t_format))
         self.assertEqual(new_d["updated_at"], u.updated_at.strftime(t_format))
-        
+
     def test_str(self):
         user = User()
         string = "[User] ({}) {}".format(user.id, user.to_dict())
